dbeee3f4ef40c55f5b12b8ba53b3b152606daabe
Parker Crosgrove <pdxparker3@gmail.com>
2023-03-16 10:44:13 -0600

    finished

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self,brand='adidas', size=0):
+        self.brand = brand
+        self.size = size
     pass
+
+    def cobble(self):
+        self.condition = "New"
+        print('Your shoe is as good as new!')
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        
+        if type(value) == int:
+            self._size = value
+
+        print("size must be an integer")
+
+    size = property(get_size, set_size)
